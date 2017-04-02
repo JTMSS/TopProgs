3922e7fed1e0a72b49297b383017127facd5ba29
JTMSS <jeremy_taylor_software@hotmail.co.uk>
2017-04-02 17:09:53 +0100

    Channels

---
+++ TopProgs/Controllers/ChannelController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TopProgs.Models;
+
+namespace TopProgs.Controllers
+{
+    public class ChannelController : Controller
+    {
+        // GET: Channel
+        public ActionResult Index()
+        {
+            //if (!HttpContext.User.Identity.IsAuthenticated)
+            //{
+            //    return RedirectToAction("Login", "Account");
+            //}
+            //else
+            //{
+                var chans = new List<Channel> { new Channel { Name = "Channel 1", Abbrv = "Ch1", ID = 1 },
+                                                new Channel { Name = "Channel 2", Abbrv = "Ch2", ID = 2 } };
+
+                return View(chans);
+            //}
+        }
+    }
+}
---
+++ TopProgs/Models/ChannelModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopProgs.Models
+{
+    /// <summary>
+    /// Channel information
+    /// </summary>
+    public class Channel
+    {
+        /// <summary>
+        /// Channel name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Channel abbreviation
+        /// </summary>
+        public string Abbrv { get; set; }
+
+        /// <summary>
+        /// Channel ID
+        /// </summary>
+        public int ID { get; set; }
+    }
+}
